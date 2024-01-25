fe6fcb5ffd4fb863c2c677aa4aed2f484566afb4
mlbd <mlimonbd@gmail.com>
2024-01-26 00:11:30 +0600

    update for vercel

--- index.js
+++ index.js
@@ -74,35 +74,8 @@ app.post('/generate-image', async (req, res) => {
         const file_ext = getFileExtensionFromUrl(thumbnail_url);
         let filename = post_id + '.' + file_ext;
 
-        const thumbnailImage = await loadImage(thumbnail_url);
 
-        const canvas = createCanvas(thumbnailImage.width, thumbnailImage.height);
-        const ctx = canvas.getContext('2d');
-
-        ctx.drawImage(thumbnailImage, 0, 0);
-
-        if (position_data) {
-            let { x, y, width, height, angle } = position_data;
-
-            const logoImage = await loadImage(logo);
-
-             // Use the original width and height of the logo
-             const originalWidth = logoImage.width;
-             const originalHeight = logoImage.height;
-
-             const newHeight = aspect_height(originalWidth, originalHeight, width);
-             const newY =  aspectY(newHeight, height, y);
-
-             ctx.save();
-             ctx.translate(x + width / 2, newY + newHeight / 2);
-             ctx.rotate(angle);
-             ctx.drawImage(logoImage, -width / 2, -newHeight / 2, width, newHeight);
-             ctx.restore();
-        }
-
-        const dataUrl = canvas.toDataURL('image/png');
-
-        res.json({ thumbnail_url, filename, dataUrl });
+        res.json({ thumbnail_url, filename });
     } catch (error) {
         console.error('Error:', error);
 
