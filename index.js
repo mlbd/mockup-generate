2c178e2b23a6fc29e8c01933df2bda156535408f
mlbd <mlimonbd@gmail.com>
2024-01-26 00:49:20 +0600

    add code into post

--- index.js
+++ index.js
@@ -5,7 +5,26 @@ const app = express();
 app.use(express.json());
 
 app.post("/", (req, res) => {
-  res.send("Express on Vercel");
+    try {
+        const { thumbnail_url, position_data, post_id, logo } = req.body;
+
+        // Log received data for debugging
+        // console.log('Received thumbnail_url:', thumbnail_url);
+        // console.log('Received position_data:', position_data);
+        // console.log('Received post_id:', post_id);
+        // console.log('Received logo:', logo);
+
+        const file_ext = getFileExtensionFromUrl(thumbnail_url);
+        let filename = post_id + '.' + file_ext;
+
+
+        res.json({ thumbnail_url, filename });
+    } catch (error) {
+        console.error('Error:', error);
+
+        res.setHeader('Content-Type', 'application/json');
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
 app.listen(5000, () => {
